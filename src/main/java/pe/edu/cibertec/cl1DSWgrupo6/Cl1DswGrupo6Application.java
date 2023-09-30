8428f009344ca506791b9794177e16dc788215d6
Zafester <nickmanv2004@gmail.com>
2023-09-30 18:22:32 -0500

    base

---
+++ src/main/java/pe/edu/cibertec/cl1DSWgrupo6/Cl1DswGrupo6Application.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.cl1DSWgrupo6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cl1DswGrupo6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cl1DswGrupo6Application.class, args);
+	}
+
+}
